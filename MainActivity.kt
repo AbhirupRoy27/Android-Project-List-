3f8a124f7905a65a84cc3ec8d81b0dc077f71acc
Abhirup Roy <109097472+AbhirupRoy27@users.noreply.github.com>
2024-04-25 20:26:26 +0530

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.project1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.project1.ui.theme.Project1Theme
+import eu.tutorials.myshoppinglistapp.ShoppingListApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Project1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ShoppingListApp()
+                }
+            }
+        }
+    }
+}
